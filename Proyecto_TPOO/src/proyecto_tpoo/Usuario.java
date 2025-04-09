5c0958fc6310913ef2f0fce07c9c2979d21a20fb
NilberAvila <n00362400@upn.pe>
2025-04-08 22:36:20 -0500

    Se corrigio la clase usuario encapsulando todos los atributos

--- Proyecto_TPOO/src/proyecto_tpoo/Usuario.java
+++ Proyecto_TPOO/src/proyecto_tpoo/Usuario.java
@@ -9,5 +9,58 @@ package proyecto_tpoo;
  * @author apnil
  */
 public class Usuario {
-    
+    private String usuario;
+    private String contraseña;
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+
+    public Usuario(String usuario, String contraseña, String nombres, String apellidos, String telefono) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    // Getters y Setters
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
